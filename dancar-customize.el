7f002dd15e3bae87d12680cfa1592fd6a627826c
Dan Carmon <dan.carmon@trusteer.com>
2013-03-07 12:56:29 +0200

    Snippets, mainly

--- dancar-customize.el
+++ dancar-customize.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(ac-auto-show-menu nil)
  '(ac-delay 0.0)
- '(ac-trigger-key "")
+ '(ac-trigger-key nil)
  '(ac-use-fuzzy t)
  '(auto-save-default nil)
  '(auto-save-file-name-transforms (quote ((".*" "~/.emacs.d/autosaves/\\1" t))))
