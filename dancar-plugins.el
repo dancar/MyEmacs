f5b1ca2f27bd3e2128232bb47de12b4585c93b56
Dan Carmon <dan@carmon.org.il>
2013-03-16 16:25:52 +0200

    Fix Ruby indentation

--- dancar-plugins.el
+++ dancar-plugins.el
@@ -82,6 +82,25 @@
 (add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 
+;; fix Ruby indentation:
+;; https://gist.github.com/fujin/5173680
+(setq ruby-deep-indent-paren nil)
+(defadvice ruby-indent-line (after unindent-closing-paren activate)
+  (let ((column (current-column))
+        indent offset)
+    (save-excursion
+      (back-to-indentation)
+      (let ((state (syntax-ppss)))
+        (setq offset (- column (current-column)))
+        (when (and (eq (char-after) ?\))
+                   (not (zerop (car state))))
+          (goto-char (cadr state))
+          (setq indent (current-indentation)))))
+    (when indent
+      (indent-line-to indent)
+      (when (> offset 0) (forward-char offset)))))
+
+
 ;; lua-mode
 (autoload 'lua-mode "lua-mode" "Lua editing mode." t)
 (add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
