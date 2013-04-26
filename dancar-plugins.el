3a8b3a046088e441fb010bfa6e94c8d7cbe8313b
dan <dan@dan-t60.(none)>
2013-04-26 12:29:18 +0300

    Open as root

--- dancar-plugins.el
+++ dancar-plugins.el
@@ -241,4 +241,18 @@ vi style of % jumping to matching brace."
                 (goto-char beg)
                 (while (< (point) end)
                   (join-line 1)))))
+
+;; sudo-open file: http://emacs-fu.blogspot.co.il/2013/03/editing-with-root-privileges-once-more.html
+(defun djcb-find-file-as-root ()
+  "Like `ido-find-file, but automatically edit the file with
+root-privileges (using tramp/sudo), if the file is not writable by
+user."
+  (interactive)
+  (let ((file (ido-read-file-name "Edit as root: ")))
+    (unless (file-writable-p file)
+      (setq file (concat "/sudo:root@localhost:" file)))
+    (find-file file)))
+;; or some other keybinding...
+(global-set-key (kbd "C-x F") 'djcb-find-file-as-root)
+
 (provide 'dancar-plugins)
