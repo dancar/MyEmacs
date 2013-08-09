16318e1740161751450f3d6ed20284e3970e288f
dan <dan@carmon.org.il>
2013-08-09 08:39:58 +0300

    moved various packages to elpa
    multiple-cursors
    mark-multiple
    expand-region
    magit
    yaml-mode
    maxframe
    helm
    auto-complete
    scss-mode
    exec-path-from-shell
    lua-mode
    yasnippet
    php-mode
    markdown-mode
    autopair
    coffee-mode
    deft

---
+++ elpa/auto-complete-20130724.1750/auto-complete-autoloads.el
@@ -0,0 +1,29 @@
+;;; auto-complete-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (auto-complete) "auto-complete" "auto-complete.el"
+;;;;;;  (20996 32740 5798 660000))
+;;; Generated autoloads from auto-complete.el
+
+(autoload 'auto-complete "auto-complete" "\
+Start auto-completion at current point.
+
+\(fn &optional SOURCES)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("auto-complete-config.el" "auto-complete-pkg.el")
+;;;;;;  (20996 32740 77023 12000))
+
+;;;***
+
+(provide 'auto-complete-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; auto-complete-autoloads.el ends here
