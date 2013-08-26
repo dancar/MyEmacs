6b4b3f010afd9c63f31e4fee6e31c1a7c68ab9ae
Dan Carmon <dan.carmon@trusteer.com>
2013-08-26 10:13:46 +0300

    idle-highlight-mode + my-coding-hook

--- dancar-plugins.el
+++ dancar-plugins.el
@@ -38,6 +38,22 @@
 (load-plugin 'move-lines)
 (load-plugin 'browse-kill-ring)
 
+
+;;;;;; stolen from idle-highlight-mode package info page:
+;;; TODO: relocate?
+(defun my-coding-hook ()
+  (make-local-variable 'column-number-mode)
+  (column-number-mode t)
+  (if window-system (hl-line-mode t))
+  (idle-highlight-mode t)
+  )
+
+;;; TODO: add?
+(add-hook 'emacs-lisp-mode-hook 'my-coding-hook)
+(add-hook 'ruby-mode-hook 'my-coding-hook)
+(add-hook 'js2-mode-hook 'my-coding-hook)
+
+
 ;; (load-plugin `f3)
 ;; Major modes:
 (load-plugin 'scss-mode)
