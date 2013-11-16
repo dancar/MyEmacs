b47711beaa17535e60bd6963a50242c95ac9eff4
Dan Carmon <dan.carmon@trusteer.com>
2013-11-16 12:36:08 +0200

    misc elpa

---
+++ elpa/diff-hl-20131114.1053/diff-hl-pkg.el
@@ -0,0 +1,2 @@
+(define-package "diff-hl" "20131114.1053" "Highlight uncommitted changes [github]"
+  '((cl-lib "0.2")))
---
+++ elpa/icicles-20131108.1624/icicles-pkg.el
@@ -0,0 +1,1 @@
+(define-package "icicles" "20131108.1624" "Minibuffer input completion and cycling. [wiki]" 'nil)
