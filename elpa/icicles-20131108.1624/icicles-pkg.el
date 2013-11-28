c6923ab62330a49e58d41fd484d00038a6a37121
Dan Carmon <dan.carmon@trusteer.com>
2013-11-28 10:10:09 +0200

    elpa updates

--- elpa/icicles-20131108.1624/icicles-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "icicles" "20131108.1624" "Minibuffer input completion and cycling. [wiki]" 'nil)
---
+++ elpa/icicles-20131127.1719/icicles-pkg.el
@@ -0,0 +1,1 @@
+(define-package "icicles" "20131127.1719" "Minibuffer input completion and cycling." 'nil)
