16919591e9eff19e96e4c1260a2d2bacb48a4337
Dan Carmon <dan.carmon@trusteer.com>
2013-12-29 15:56:55 +0200

    Ruby files opened in ruby-mode

--- dancar-plugins.el
+++ dancar-plugins.el
@@ -104,12 +104,12 @@
 ;;; (define-key ac-menu-map "\C-m" 'ac-complete)
 ;
 ;;; ruby-mode special files:
-;(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
-;(add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
-;(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
-;(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
-;(add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
-;(add-to-list 'auto-mode-alist '("capfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("capfile" . ruby-mode))
 ;
 ;;; fix Ruby indentation:
 ;;; https://gist.github.com/fujin/5173680
