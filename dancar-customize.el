4438cf747d0e4175a3b322187cda0f0de598fcb5
Dan Carmon <dan.carmon@trusteer.com>
2014-01-09 10:00:25 +0200

    Bright background

--- dancar-customize.el
+++ dancar-customize.el
@@ -114,6 +114,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "azure2" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :foundry "apple" :family "Monaco"))))
  '(highlight-symbol-face ((t (:underline "Blue"))))
  '(idle-highlight ((t (:inherit region :background "RoyalBlue4"))))
  '(magit-item-highlight ((t (:background "gray96"))))
