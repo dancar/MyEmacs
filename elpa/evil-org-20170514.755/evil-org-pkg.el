d2f607ff01f52dd2b584b24422ecc9a769660607
Dan Carmon <dan@carmon.org.il>
2017-06-21 13:48:15 +0700

    elpa updates

--- elpa/evil-org-20170514.755/evil-org-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "evil-org" "20170514.755" "evil keybindings for org-mode" '((emacs "24.4") (evil "1.0") (org "8.0.0")) :commit "25d159750fe90b9b42e2edf6390752e18b80e021" :url "https://github.com/Somelauw/evil-org-mode.git" :keywords '("evil" "vim-emulation" "org-mode" "key-bindings" "presets"))
---
+++ elpa/evil-org-20170611.1149/evil-org-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "evil-org" "20170611.1149" "evil keybindings for org-mode" '((emacs "24.4") (evil "1.0") (org "8.0.0")) :commit "4da3d179d71113b906173a974fec933e9f55afa8" :url "https://github.com/Somelauw/evil-org-mode.git" :keywords '("evil" "vim-emulation" "org-mode" "key-bindings" "presets"))
