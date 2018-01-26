0a0d87e84572ac116cdbcaca5c6015824e0f49c4
Dan Carmon <dancarmon@Dans-MacBook-Air.fritz.box>
2018-01-26 03:49:32 +0100

    Updated 50+ packages

--- elpa/treemacs-evil-20170916.1443/treemacs-evil-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "treemacs-evil" "20170916.1443" "Evil mode integration for treemacs" '((evil "1.2.12") (treemacs "0")) :commit "e6ac5a00c32e5f291d2839518bfa6c70f72c727c" :url "https://github.com/Alexander-Miller/treemacs")
--- elpa/treemacs-evil-20170925.2232/treemacs-evil-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "treemacs-evil" "20170925.2232" "Evil mode integration for treemacs" '((evil "1.2.12") (treemacs "0")) :commit "0adf26aecc14c477cf21cd71774db1d3765ab38c" :url "https://github.com/Alexander-Miller/treemacs")
