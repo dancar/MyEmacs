a0690a29050fc66d9128d270981286804eaf94ec
Dan <dan@carmon.org.il>
2018-06-04 09:11:31 +0200

    elpa update all the things

--- elpa/treemacs-evil-20180504.417/treemacs-evil-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "treemacs-evil" "20180504.417" "Evil mode integration for treemacs" '((evil "1.2.12") (treemacs "0")) :commit "2991b00f7c93c80bbcd2d2fd31ed663b15817b2a" :url "https://github.com/Alexander-Miller/treemacs")
