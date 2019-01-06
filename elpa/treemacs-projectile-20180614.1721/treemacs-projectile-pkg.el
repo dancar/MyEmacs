4f657123e9b17be9ae5fbe4f14fbba9fefe6f4b7
Dan Carmon <dan@carmon.org.il>
2019-01-06 14:45:53 +0100

    43 updated elpas

--- elpa/treemacs-projectile-20180614.1721/treemacs-projectile-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "treemacs-projectile" "20180614.1721" "Projectile integration for treemacs" '((projectile "0.14.0") (treemacs "0")) :commit "9a27a15faabcddfde1157bbcba453b9e18def194" :authors '(("Alexander Miller" . "alexanderm@web.de")) :maintainer '("Alexander Miller" . "alexanderm@web.de") :url "https://github.com/Alexander-Miller/treemacs")
