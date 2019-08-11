777f9d6a0d0dcf1cf0740a725b0eea505be8f35b
Dan <dan@carmon.org.il>
2019-08-11 17:38:39 +0200

    auto-update elpa (paradox)

---
+++ elpa/graphql-mode-20190731.1825/graphql-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "graphql-mode" "20190731.1825" "Major mode for editing GraphQL schemas" '((emacs "24.3")) :stars '(#("75" 0 2 (font-lock-face paradox-star-face))) :commit "e757919fe3cf6d687d5d970662964fc37115b1d6" :keywords '("languages") :authors '(("David Vazquez Pua" . "davazp@gmail.com")) :maintainer '("David Vazquez Pua" . "davazp@gmail.com"))
