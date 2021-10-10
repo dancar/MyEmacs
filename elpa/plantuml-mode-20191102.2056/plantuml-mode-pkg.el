2295d116c5261b399b1b73f9de279350549f966d
Dan Carmon <dan@carmon.org.il>
2021-10-10 17:27:01 +0300

    misc elpa

---
+++ elpa/plantuml-mode-20191102.2056/plantuml-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "plantuml-mode" "20191102.2056" "Major mode for PlantUML" '((dash "2.0.0") (emacs "25.0")) :stars '(#("205" 0 3 (font-lock-face paradox-star-face))) :commit "ea45a13707abd2a70df183f1aec6447197fc9ccc" :authors '(("Zhang Weize (zwz)")) :maintainer '("Carlo Sciolla (skuro)") :keywords '("uml" "plantuml" "ascii"))
